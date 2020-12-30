c2157b9950876add7940a30dd865f156f0f0c8ac
Kyle Stubbs <kylestubbs@ucsb.edu>
2020-12-29 21:57:17 -0800

    Put in URL into file, extract that file into string, set up basic parsing with Jsoup

--- app/src/main/java/edu/ucsb/cs/cs184/sportsscores/ui/home/nbaFragment.kt
+++ app/src/main/java/edu/ucsb/cs/cs184/sportsscores/ui/home/nbaFragment.kt
@@ -1,27 +1,72 @@
 package edu.ucsb.cs.cs184.sportsscores.ui.home
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.squareup.picasso.Picasso
 import edu.ucsb.cs.cs184.sportsscores.R
+import org.jsoup.Jsoup
+import java.io.*
+import java.net.HttpURLConnection
+import java.net.URL
+import kotlin.concurrent.thread
+
 
 class nbaFragment : Fragment() {
 
-    private lateinit var nbaViewModel: nbaViewModel
+    private lateinit var viewModel: nbaViewModel
+
+    private lateinit var imageView: ImageView
+    private lateinit var textView: TextView
 
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        nbaViewModel =
+        viewModel =
                 ViewModelProvider(this).get(nbaViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_nba, container, false)
+        imageView = root.findViewById(R.id.image)
+        textView = root.findViewById(R.id.text)
+        retrieveWebInfo()
         return root
     }
+
+    private fun retrieveWebInfo() {
+//        val url = URL("https://www.espn.com/nba/scoreboard/_/date/20201229")
+//        val con = url.openConnection() as HttpURLConnection
+//        val datas = con.inputStream.bufferedReader().readText()
+//        val docParse = Jsoup.parse(datas)
+//        val outputStreamWriter = OutputStreamWriter(requireContext().openFileOutput("nba1.txt", Context.MODE_APPEND))
+//        outputStreamWriter.write(datas)
+//        outputStreamWriter.close()
+        val test = activity?.filesDir?.absolutePath
+        val file = File(test + "/nba1.txt")
+        val iss = FileInputStream(file)
+        val lin = BufferedReader(InputStreamReader(iss))
+        val s = lin.readText()
+
+        val doc = Jsoup.parse(s)
+
+        val imageElements = doc.select("img[src*=500]")
+//      val imageElements = doc.getElementsByClass("teamlogo")
+        val textElements = doc.getElementsByTag("h1")
+
+        val imageUrl = imageElements[0].absUrl("src")
+
+        textView.text = textElements[1].text()
+        Picasso.get().load(imageUrl).into(imageView)
+}
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+    }
+    // might not need
 }
